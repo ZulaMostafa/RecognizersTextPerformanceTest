7d1f8575cd94b8c38d024d88685b50ed8bd8f12e
Salah Mostafa <v-samostafa@microsoft.com>
2021-11-15 13:41:19 +0200

    [.Net] Added config model

---
+++ .Net/RecognizersTextPerformanceTest/ViewModels/ConfigModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Recognizers.Text;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecognizersTextPerformanceTest.ViewModels
+{
+    public class ConfigModel
+    {
+        [JsonProperty("rootPath")]
+        public string RootPath { get; set; }
+        
+        [JsonProperty("cultures")]
+        public List<Culture> cultures { get; set; }
+
+        [JsonProperty("recognizers")]
+        public List<string> recognizers { get; set; }
+    }
+}
