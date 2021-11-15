00d9ea5a615453191b8db0a0e9084884d4f10a43
Salah Mostafa <v-samostafa@microsoft.com>
2021-11-15 18:01:30 +0200

    added loading tests for python

--- Python/runner.py
+++ Python/runner.py
@@ -1,3 +1,4 @@
+import json
 import constants
 import os
 import services.configs_reader as configs_reader
@@ -6,6 +7,7 @@ from services.text_recognizers_client import text_recognizers_client
 from services.logging_service import logging_service
 from models.Logger.console_logger import console_logger
 from models.Logger.file_logger import file_logger
+from operator import attrgetter
 
 # load configs file
 configs = configs_reader.load_application_configs()
@@ -16,11 +18,23 @@ text_recognizers_client = text_recognizers_client(configs["cultures"], configs["
 # create performance model
 performance_model = performance_model()
 
+# load tests [TODO: enhance loading tests for python]
+allTests = []
+for culture in configs["cultures"]: 
+    path = os.path.join(configs["rootPath"], f'{culture}.json')
+
+    with open(path) as f:
+        tests = f.read() 
+
+    json_data = json.loads(tests)
+    test = [o['Input'] for o in json_data]
+    allTests.extend(test)
+print(allTests)
+
+
 # init logging service
 logging_service = logging_service()
 
 # register loggers [TODO: add types to configs file]
 logging_service.add_logger(console_logger())
 logging_service.add_logger(file_logger())
-
-logging_service.log('test', 'double test')
--- Python/services/text_recognizers_client.py
+++ Python/services/text_recognizers_client.py
@@ -7,60 +7,56 @@ from recognizers_sequence import SequenceRecognizer
 
 class text_recognizers_client:
     def __init__(self, cultures, recognizers):
-        self.models = []
-        
-        print(cultures)
-        print(recognizers)
+        self.__models = []
 
         for culture in cultures:
             if 'Choice' in recognizers:
-                self.add_choice_models(culture)
+                self._add_choice_models(culture)
 
             if 'DateTime' in recognizers:
-                self.add_date_time_models(culture)
+                self._add_date_time_models(culture)
 
             if 'Number' in recognizers:
-                self.add_number_recognizers_models(culture)
+                self._add_number_recognizers_models(culture)
             
             if 'NumberWithUnit' in recognizers:
-                self.add_number_with_unit_recognizers_models(culture)
+                self._add_number_with_unit_recognizers_models(culture)
 
             if 'Sequence' in recognizers:
-                self.add_sequence_models(culture)
-        
+                self._add_sequence_models(culture)
         
 
-    def add_choice_models(self, culture):
+    def _add_choice_models(self, culture):
         choice_recognizer = ChoiceRecognizer(culture)
-        self.models.append(choice_recognizer.get_boolean_model())
+        self.__models.append(choice_recognizer.get_boolean_model())
 
-    def add_date_time_models(self, culture):
+    def _add_date_time_models(self, culture):
         date_time_recognizer = DateTimeRecognizer(culture)
-        self.models.append(date_time_recognizer.get_datetime_model())
+        self.__models.append(date_time_recognizer.get_datetime_model())
 
-    def add_number_recognizers_models(self, culture):
+    def _add_number_recognizers_models(self, culture):
         print(culture)
         number_recognizer = NumberRecognizer(culture)
-        self.models.append(number_recognizer.get_number_model())
-        self.models.append(number_recognizer.get_ordinal_model())
-        self.models.append(number_recognizer.get_percentage_model())
+        self.__models.append(number_recognizer.get_number_model())
+        self.__models.append(number_recognizer.get_ordinal_model())
+        self.__models.append(number_recognizer.get_percentage_model())
 
-    def add_number_with_unit_recognizers_models(self, culture):
+    def _add_number_with_unit_recognizers_models(self, culture):
         number_with_unit_recognizer = NumberWithUnitRecognizer(culture)
-        self.models.append(number_with_unit_recognizer.get_age_model())
-        self.models.append(number_with_unit_recognizer.get_currency_model())
-        self.models.append(number_with_unit_recognizer.get_dimension_model())
-        self.models.append(number_with_unit_recognizer.get_temperature_model())
+        self.__models.append(number_with_unit_recognizer.get_age_model())
+        self.__models.append(number_with_unit_recognizer.get_currency_model())
+        self.__models.append(number_with_unit_recognizer.get_dimension_model())
+        self.__models.append(number_with_unit_recognizer.get_temperature_model())
 
-    def add_sequence_models(self, culture):
+    def _add_sequence_models(self, culture):
         sequence_recognizer = SequenceRecognizer(culture)
-        self.models.append(sequence_recognizer.get_phone_number_model())
-        self.models.append(sequence_recognizer.get_ip_address_model())
-        self.models.append(sequence_recognizer.get_mention_model())
-        self.models.append(sequence_recognizer.get_hashtag_model())
-        self.models.append(sequence_recognizer.get_url_model())
-        self.models.append(sequence_recognizer.get_guid_model())
-        self.models.append(sequence_recognizer.get_email_model())
+        self.__models.append(sequence_recognizer.get_phone_number_model())
+        self.__models.append(sequence_recognizer.get_ip_address_model())
+        self.__models.append(sequence_recognizer.get_mention_model())
+        self.__models.append(sequence_recognizer.get_hashtag_model())
+        self.__models.append(sequence_recognizer.get_url_model())
+        self.__models.append(sequence_recognizer.get_guid_model())
+        self.__models.append(sequence_recognizer.get_email_model())
 
     def run_test(self, input):
         for model in self.models:
