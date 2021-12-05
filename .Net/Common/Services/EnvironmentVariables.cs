62156ad71cfd9df8c57f50c58c4bea57ba9b63c3
Salah Mostafa <v-samostafa@microsoft.com>
2021-12-05 17:22:14 +0200

    fix

--- .Net/Common/Services/EnvironmentVariables.cs
+++ .Net/Common/Services/EnvironmentVariables.cs
@@ -10,8 +10,6 @@ namespace Common.Services
         private const string RecognizersConfiguration = "TR_RECOGNIZERS";
         private const string OperationName = "TR_OPERATION_NAME";
         private const string MainDirectory = "TR_MAIN_DIRECTORY";
-        private const string CurrentReleaseDirectory = "TR_CURRENT_RELEASE_DIRECTORY";
-        private const string NextReleaseDirectory = "TR_NEXT_RELEASE_DIRECTORY";
 
         public static string GetCulturesConfiguration() 
             => Environment.GetEnvironmentVariable(CulturesConfiguration);
@@ -21,9 +19,5 @@ namespace Common.Services
             => Environment.GetEnvironmentVariable(OperationName);
         public static string GetMainDirectory()
             => Environment.GetEnvironmentVariable(MainDirectory);
-        public static string GetCurrentReleaseDirectory()
-            => Environment.GetEnvironmentVariable(CurrentReleaseDirectory);
-        public static string GetNextReleaseDirectory()
-            => Environment.GetEnvironmentVariable(NextReleaseDirectory);
     }
 }
---
+++ .net/ResultsFinalizer/Program.cs
@@ -0,0 +1,25 @@
+﻿using Common.Helpers;
+using Common.Services;
+using ResultsFinalizer.Helpers;
+using System;
+using System.IO;
+
+namespace ResultsFinalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainDirectory = EnvironmentVariables.GetMainDirectory();
+
+            var currentReleasePath = Path.Combine(mainDirectory, "CurrentRelease");
+            var currentReleaseResults = CSVToBenchmarkResults.Load(currentReleasePath);
+
+            var nextReleasePath = Path.Combine(mainDirectory, "NextRelease");
+            var nextReleaseResults = CSVToBenchmarkResults.Load(nextReleasePath);
+
+            FinalResultsToCSV.Save(currentReleaseResults, nextReleaseResults, "results");
+        }
+    }
+}
+
