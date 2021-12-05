498d44767a1da59a9f57ebbbe8d8a310261883fb
Salah Mostafa <v-samostafa@microsoft.com>
2021-12-05 19:34:40 +0200

    Added logging

--- .Net/ResultsFinalizer/Program.cs
+++ .Net/ResultsFinalizer/Program.cs
@@ -14,11 +14,14 @@ namespace ResultsFinalizer
 
             var currentReleasePath = Path.Combine(mainDirectory, "CurrentRelease");
             var currentReleaseResults = CSVToBenchmarkResults.Load(currentReleasePath);
+            Console.WriteLine("Current release tests loaded");
 
             var nextReleasePath = Path.Combine(mainDirectory, "NextRelease");
             var nextReleaseResults = CSVToBenchmarkResults.Load(nextReleasePath);
+            Console.WriteLine("next release tests loaded");
 
             FinalResultsToCSV.Save(currentReleaseResults, nextReleaseResults, "results");
+            Console.Write("Results Saved");
         }
     }
 }
